31986d3b4b8a03b885e6d72ce5bbc02e79e2b969
project-team <marco.leo@prjteam.com>
2015-07-01 15:35:30 +0200

    methods

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 var mongojs = require('mongojs')
 
-module.exports = {
-	insert: function(){
+module.exports = function(dbname, collection){
 
-	},
-	changeStatus: function(){
+	return {
+		insert: function(asset){
+			var asset = getDbCollection(dbname, collection);
 
-	},
-	getStatus: function(){
+			asset.insert(asset);
+		},
+		changeStatus: function(id, status){
 
+			var asset = getDbCollection(dbname, collection);
+
+			asset.update({id:id}, {status : status}, {multi:true}, function() {
+	    	// the update is complete
+			});
+		},
+		getStatus: function(id){
+			var asset = getDbCollection(dbname, collection);
+
+			// find a document using a native ObjectId
+			asset.findOne({
+			    id:id
+			}, function(err, doc) {
+			    return doc.status;
+			});
+
+
+		}
 	}
+
+}
+
+function getDbCollection(dbname, collection){
+
+	//TODO gestire errori
+	var db = mongojs(dbname);
+	var mycollection = db.collection(collection);
+
+	return collection;
 }
