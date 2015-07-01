89f3c2823686b209f02faa1fef1e13bba15a52ff
Marco Bonezzi <marco.bonezzi@hotmail.it>
2015-07-01 17:38:48 +0200

    Revert "fix test"
    This reverts commit ea6cde06998d694d39c3ef45781721bcd6005d9b.

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
-var assert = require('assert');
+
 var asset_manager = require('./')('corsonode','asset')
 
-describe("asset-manager", function(){  
-  it("save in db", function(done){    
-    asset_manager.put({name: 'prova', status: 1}, function (err, result) {
-      if (err) return done(err)
-       asset_manager.get({name: result.name, status: result.status}, function (err, result2) {
-        if (err) return done(err)
-         assert.deepEqual(result, result2)
-         done()
-       })
-    });  
-  });
+asset_manager.put({name : "testttttt", status: 4},function(error,data){
+
+  console.log(data.name);
+  asset_manager.close();
+
 });
